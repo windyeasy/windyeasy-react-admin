040bf9898890f1e795e35870a2084ac747520343
windyeasy <windyeasy13gb@163.com>
2024-01-04 14:21:36 +0800

    fix: 修复开发时图标不显示

--- craco.config.js
+++ craco.config.js
@@ -11,9 +11,9 @@ module.exports = {
   },
   devServer: {
     proxy: {
-      '/': {
+      '/api': {
         target: 'http://localhost:8000',
-        pathRewrite: { '^/': '' }
+        pathRewrite: { '^/api': '' }
       }
     }
   }
