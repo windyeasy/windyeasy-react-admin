406486638278f96616d8cbeb7c308a00e032a225
windyeasy <windyeasy13gb@163.com>
2024-01-05 14:19:34 +0800

    feat: 用户表查询添加状态筛选

--- src/pages/main/system/user/config/search.config.tsx
+++ src/pages/main/system/user/config/search.config.tsx
@@ -14,6 +14,17 @@ export const searchConfig: WBaseFormItem[] = [
     placeholder: '请输入用户昵称'
   },
   {
+    type: 'select',
+    label: '用户状态',
+    prop: 'state',
+    defaultValueUn: true,
+    placeholder: '请选择用户状态',
+    options: [
+      { label: '启用', value: 1 },
+      { label: '禁用', value: 0 }
+    ]
+  },
+  {
     type: 'input',
     label: '手机号码',
     prop: 'telephone',
