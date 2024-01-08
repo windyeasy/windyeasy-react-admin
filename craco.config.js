4db555c2321845637c37fa00e7d5135e3e01bfc9
windyeasy <windyeasy13gb@163.com>
2024-01-08 19:46:17 +0800

    fix: 修复依赖bug

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const CracoLessPlugin = require('craco-less')
 const resolve = (pathname) => path.resolve(__dirname, pathname)
+
 module.exports = {
   plugins: [{ plugin: CracoLessPlugin }],
   // webpack
